45e0f210d1e6c19a55c4ea730a159b2a2ac50b60
Aleix Canet <acanet94@gmail.com>
2024-08-16 21:59:56 +0200

    feat: add shell + page header components

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,36 +1,33 @@
 import Link from "next/link"
+import { GitHubLogoIcon, MixIcon } from "@radix-ui/react-icons"
 
 import { siteConfig } from "@/config/site"
 import { Button } from "@/components/ui/button"
-import { Icons } from "@/components/icons"
-import { ThemeSwitcher } from "@/components/theme-switcher"
+
+import { ModeToggle } from "../mode-toggle"
 
 export function SiteHeader() {
   return (
-    <header className="w-full bg-background pt-4">
-      <div className="container flex h-16 max-w-3xl items-center justify-between">
-        <Link href="/" className="font-bold">
-          {siteConfig.name}
-          <span className="sr-only">Home</span>
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 items-center">
+        <Link href="/" className="mr-2 flex items-center md:mr-6 md:space-x-2">
+          <MixIcon className="size-4" aria-hidden="true" />
+          <span className="hidden font-bold md:inline-block">
+            {siteConfig.name}
+          </span>
         </Link>
-        <nav className="flex items-center space-x-1">
-          <Button variant="ghost" size="icon" className="size-7" asChild>
+        <nav className="flex flex-1 items-center md:justify-end">
+          <Button variant="ghost" size="icon" className="size-8" asChild>
             <Link
-              href={siteConfig.links.githubProfile}
+              aria-label="GitHub repo"
+              href={siteConfig.links.github}
               target="_blank"
-              rel="noreferrer"
+              rel="noopener noreferrer"
             >
-              <Icons.gitHub className="size-4" />
-              <span className="sr-only">GitHub</span>
+              <GitHubLogoIcon className="size-4" aria-hidden="true" />
             </Link>
           </Button>
-          <Button variant="ghost" size="icon" className="size-7">
-            <Link href={siteConfig.links.x} target="_blank" rel="noreferrer">
-              <Icons.x className="size-3 fill-current" />
-              <span className="sr-only">X</span>
-            </Link>
-          </Button>
-          <ThemeSwitcher />
+          <ModeToggle />
         </nav>
       </div>
     </header>
--- src/config/site.ts
+++ src/config/site.ts
@@ -3,11 +3,14 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "Your site name",
   description: "The description of the site",
-  url: "https://your-site-rul.dev",
+  url:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://your-side.dev",
   ogImage: "https://your-site-rul.dev/opengraph-image.png",
   links: {
     x: "https://twitter.com/your-username",
-    github: "https://github.com/your-username/your-site-rul.dev",
+    github: "https://github.com/your-username/your-site-repo.dev",
     githubProfile: "https://github.com/your-username",
     discord: "https://discord.com/users/your-username",
     calDotCom: "https://cal.com/your-username",
